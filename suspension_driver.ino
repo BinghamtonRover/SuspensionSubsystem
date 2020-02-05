d87e452cc8b3e6c55c8b5bbb6add5dc68b365375
Layne Gustafson <lgustaf1@binghamton.edu>
2020-02-05 14:50:22 -0500

    Add relay control

--- suspension_driver.ino
+++ suspension_driver.ino
@@ -3,6 +3,9 @@
 const uint8_t ROCS_ID = 0x01;
 const char ROCS_NAME[] = "suspension";
 
+const int L_RELAY_PIN = 12;
+const int R_RELAY_PIN = 13;
+
 const int L_DIR_PIN = 11;
 const int L_SPEED_PIN = 9;
 
@@ -14,14 +17,36 @@ const uint8_t MAX_SPEED = 0;
 
 #define RECEIVE_TIMEOUT 1000
 
+#define RELAY_ACTIVE_SIGNAL HIGH
+#define RELAY_INACTIVE_SIGNAL LOW
+
 uint32_t last_receive_time;
+
+bool enabled = true;
 
 static void stop() {
     analogWrite(L_SPEED_PIN, MIN_SPEED);
     analogWrite(R_SPEED_PIN, MIN_SPEED);
 }
 
+static void disable() {
+    enabled = false;
+
+    digitalWrite(L_RELAY_PIN, RELAY_INACTIVE_SIGNAL);  
+    digitalWrite(R_RELAY_PIN, RELAY_INACTIVE_SIGNAL);  
+}
+
+static void enable() {
+    digitalWrite(L_RELAY_PIN, RELAY_ACTIVE_SIGNAL);  
+    digitalWrite(R_RELAY_PIN, RELAY_ACTIVE_SIGNAL);  
+
+    enabled = true;
+}
+
 void setup() {
+    pinMode(L_RELAY_PIN, OUTPUT);
+    pinMode(R_RELAY_PIN, OUTPUT);
+
     pinMode(L_DIR_PIN, OUTPUT);
     pinMode(L_SPEED_PIN, OUTPUT);
 
@@ -31,6 +56,7 @@ void setup() {
     digitalWrite(L_DIR_PIN, HIGH);
     digitalWrite(R_DIR_PIN, HIGH);
 
+    enable();
     stop();
   
     rocs::init(ROCS_ID, ROCS_NAME, sizeof(ROCS_NAME) - 1);
@@ -62,6 +88,15 @@ void write_handler(uint8_t reg, uint8_t value) {
 void loop() {
     // put your main code here, to run repeatedly:
     if (millis() - last_receive_time > RECEIVE_TIMEOUT) {
-        stop();
+        if (enabled) {
+            stop();
+            disable();
+        }
+    } else {
+        if (!enabled) {
+            // We are within receive timeout, and disabled.
+            // Reenable.
+            enable();
+        }
     }
 }
